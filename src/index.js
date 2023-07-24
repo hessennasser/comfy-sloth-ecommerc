a104288723b41d94f7f92d65a8c1126833d4fad4
hessennasser <hessenaloosh@gmail.com>
2023-07-24 21:58:06 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+import { CartProvider } from './context/cart_context';
+import { UserProvider } from './context/user_context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+    <Auth0Provider
+        domain="dev-z7dzyn66ebkxeti1.us.auth0.com"
+        clientId="8CfL3siSIYAnNIuQzzNA2pt4emaHNO8n"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+        cacheLocation='localstorage'
+    >
+        <UserProvider>
+            <ProductsProvider>
+                <FilterProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </FilterProvider>
+            </ProductsProvider >
+        </UserProvider>
+    </Auth0Provider>
+);
